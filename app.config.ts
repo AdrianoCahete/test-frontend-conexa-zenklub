461691b37d71ed075cf0d26b0699e511d5f3ffbd
Adriano Cahete <me@adrianocahete.dev>
2024-06-21 20:53:44 -0300

    feat(root): base configs

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  title: "Conexa + Zenklub - Desafio Frontend [Adriano Cahete]",
+  theme: {
+    dark: false,
+  },
+
+  stylelint: {
+    /* module options */
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/styles/root.scss" as *;',
+        },
+      },
+    },
+  },
+
+  app: {
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+
+  modules: ["@nuxt/test-utils/module"],
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+
+  experimental: {
+    viewTransition: true,
+  },
+});
