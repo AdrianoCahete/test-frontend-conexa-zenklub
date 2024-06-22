799586472902a47b35797ff8979b7d3afdbcba86
Adriano Cahete <me@adrianocahete.dev>
2024-06-21 22:45:11 -0300

    feat(app): add fetch request + add content + update route config

--- app.vue
+++ app.vue
@@ -1,48 +1,10 @@
 <template>
-  <main>
-    <section class="user-profile">
-      <div class="user-avatar">
-        <picture v-if="profile.photo" :src="profile.photo">
-          <source media="(min-width:650px)" :srcset="profile.photo" />
-          <img :src="profile.photo" style="width: auto" />
-        </picture>
-        <div v-else>Sem foto</div>
-      </div>
-      <div>
-        <h1>{{ profile.name }}</h1>
-        <div>
-          <h2>Specialization</h2>
-          <h3>Place</h3>
-        </div>
-        <div>
-          <div>Stars</div>
-          <div>Reviews</div>
-        </div>
-        <div>
-          <div>Price</div>
-          <div>Price minutes</div>
-        </div>
-        <div>Description</div>
-      </div>
-    </section>
-    <section class="schedule">Agenda</section>
-    <hr />
-    <pre>{{ profile }}</pre>
-  </main>
+  <NuxtPage />
 </template>
 
-<script setup lang="ts">
-// interface Profile {
-//   name: string;
-// }
+<script setup lang="ts"></script>
 
-const { id } = useRoute().params || 1;
-const { data: profile } = await useFetch(
-  `http://localhost:3001/professional/${id}`
-);
-</script>
-
-<style>
+<style lang="scss">
 .page-enter-active,
 .page-leave-active {
   transition: all 0.4s;
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,91 @@
 <template>
-  <section class="user-profile">
-    <div class="user-avatar"></div>
-    <div>
-      <h1>Name</h1>
-      <div>
-        <h2>Specialization</h2>
-        <h3>Place</h3>
+  <div v-if="error">{{ error.message }}</div>
+  <div v-else-if="!data">Loading...</div>
+  <main v-else>
+    <section class="user-profile">
+      <div class="user-avatar">
+        <picture v-if="data.photo">
+          <source media="(min-width:650px)" :srcset="data.photo" />
+          <img :src="data.photo" />
+        </picture>
+        <div v-else>Sem foto</div>
       </div>
       <div>
-        <div>Stars</div>
-        <div>Reviews</div>
+        <h1>{{ data.name }}</h1>
+        <div>
+          <h2>{{ data.specialization }}</h2>
+          <h3>Place</h3>
+        </div>
+        <div>
+          <div>Stars</div>
+          <div>Reviews</div>
+        </div>
+        <div>
+          <div>Price</div>
+          <div>Price minutes</div>
+        </div>
+        <div>Description</div>
       </div>
-      <div>
-        <div>Price</div>
-        <div>Price minutes</div>
+    </section>
+    <section class="schedule">
+      Agenda
+      <div v-for="session in data.sessions" v-bind:key="data.sessions.id">
+        {{ session }}
       </div>
-      <div>Description</div>
-    </div>
-  </section>
-  <section class="schedule">Agenda</section>
+    </section>
+    <hr />
+  </main>
+  <pre>{{ route.params.id }}</pre>
+  id: {{ id || "null" }}
+  <pre>{{ data }}</pre>
 </template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+const route = useRoute();
 
-<style></style>
+interface Professional {
+  id: number;
+  name: string;
+  photo: string;
+  specialization: string;
+  sessions: Array;
+}
+
+const id = route.params.id || "1";
+const { data, error } = await useFetch<Professional>(
+  `http://localhost:3001/professional/${id}`
+);
+
+// const { data: count } = await useFetch("http://localhost:3001/professional/");
+</script>
+
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: blur(1rem);
+}
+
+.user-avatar {
+  img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background-color: #eee;
+    overflow: hidden;
+  }
+}
+</style>
